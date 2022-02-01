84e8b16f3de09e71d8c94244a3054bf448cee1f9
JayYong <96015600+dev-jay-yong@users.noreply.github.com>
2022-02-01 17:38:22 +0900

    Create 15_deps.js

---
+++ TID/react/15_deps.js
@@ -0,0 +1,39 @@
+import Button from "./Button";
+import styles from "./App.module.css"
+import {useState, useEffect} from "react";
+
+function App() {
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => {
+      setKeyword(event.target.value)
+    }
+    useEffect(() => {
+        console.log("I run only once.")
+    }, [])
+    useEffect(() => {
+        console.log("I run  when 'keyword' changes.", keyword)
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run  when 'counter' changes.", keyword)
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("I run  when 'counter' or 'keyword' changes.", keyword)
+    }, [counter, keyword]);
+
+    return (
+    <div>
+        <input
+        value={keyword}
+        onChange={onChange}
+        type={"text"}
+        placeholder={"Search here"}/>
+        <h1> {counter} </h1>
+        <button onClick={onClick}>click me</button>
+    </div>
+    );
+}
+
+export default App;
